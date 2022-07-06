1cf19f78f844fc527b040ff87e4204020c2850d7
Manoel Campos <manoelcampos@gmail.com>
2022-07-06 09:16:27 -0300

    Uses the same port for all operating systems for simplicity

--- util.js
+++ util.js
@@ -1,6 +1,6 @@
 const isWindows = () => process.platform === 'win32';
 
-const getPort = config => config.port || (isWindows() ? 80 : 8080);
+const getPort = config => config.port || 8080;
 
 module.exports = {
     getPort
